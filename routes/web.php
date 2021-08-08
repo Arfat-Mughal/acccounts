3dee27c4d7c08e49f126662e1b1bd530c58af780
Arfat <57156681+Arfat-Mughal@users.noreply.github.com>
2021-08-08 21:35:29 +0500

    add main layout

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
 
 Route::namespace('Auth')->group(function () {
